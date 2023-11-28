f543d14fc917aae0ec33ac58f080055f57ea42e4
julianaLeano <julexd125@gmail.com>
2023-11-28 15:35:00 -0600

    added base frontend, button

--- client/src/RelatedImage.js
+++ client/src/RelatedImage.js
@@ -1,3 +1,4 @@
+import { ImageViewer } from "react-image-viewer-dv"
 const RelatedImage = ({ path, score }) => {
   return (
     <div className="flex flex-col items-center justify-items-center">
@@ -11,9 +12,14 @@ const RelatedImage = ({ path, score }) => {
 
 const RelatedImagesContainer = ({ images }) => {
   return (
-    <div className="grid grid-cols-4 gap-4">
-      {images.map(image => <RelatedImage path={image.filename} score={image.similarity} />)}
-    </div>
+    // <div className="grid grid-cols-4 gap-4">
+    //   {images.map(image => <RelatedImage path={image.filename} score={image.similarity} />)}
+    // </div>
+      <div className ="grid grid-cols-4 gap-4">
+        {/* <img src={path}  alt="Your image" /> */}
+        {images.map(image => <ImageViewer> <RelatedImage path={image.filename} score={image.similarity} /> </ImageViewer>)}
+        
+      </div>
   );
 }
 
