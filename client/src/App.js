c6a70c06c2cf58efd4dfd96b461db1359c0c0944
julianaLeano <julexd125@gmail.com>
2023-11-28 20:58:33 -0600

    added animating title that changes fonts

--- client/src/App.js
+++ client/src/App.js
@@ -39,40 +39,32 @@ function App() {
   return (
     <div className="App">
       <div class="flex grid grid-rows-4 grid-cols-1 items-center justify-center content-between pt-64 w-full bg-screen bg-center h-screen bg-[url('/public/header_canvas.jpg')] ">
-          {/* <div class="flex grid grid-rows-2 grid-cols-1 items-center justify-center content-between pt-72"> */}
-          {/* ^^^bg-gray-900 bg-opacity-50 */}
-            <div class="text-center">
-                {/* FIXME change font, size, etc -> Blank Canvas */}
-                {/* loop thru different fonts */}
-                <h1 class="text-grey text-8xl font-semibold uppercase font-mono">Blank Canvas</h1>
-            </div>
+        <div class="text-center">
+            <h1 class="text-grey text-9xl font-fontTitle animate-change-font">Blank Canvas</h1>
+        </div>
             
-            <div class="flex items-center justify-center w-full">
-            <label for="dropzone-file" class="flex flex-col items-center justify-center h-64 w-1/2 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-transparent m-3">
-              {/*<label for="dropzone-file" class="flex flex-col items-center justify-center w-1/2 h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600 m-3">*/}
-                <div class="flex flex-col items-center justify-center pt-5 pb-6">
-                    <svg class="w-8 h-8 mb-4 text-black-500 dark:text-black-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
-                    </svg>
-                    <p class="mb-2 text-md text-black-500 dark:text-black-400"><span class="font-bold">Click to upload</span> or drag and drop</p>
-                    <p class="text-sm text-black-500 dark:text-black-400">PNG, JPG or GIF</p>
-                </div>
-                <input id="dropzone-file" type="file" class="hidden" onChange={(e) => setImageFile(e.target.files[0])}/>
-            </label>
-          </div> 
+        <div class="flex items-center justify-center w-full">
+          <label for="dropzone-file" class="flex flex-col items-center justify-center h-64 w-1/2 border-2 border-gray-400 border-dashed rounded-lg cursor-pointer bg-transparent m-3">
+              <div class="flex flex-col items-center justify-center pt-5 pb-6">
+                  <svg class="w-12 h-12 mb-4 text-black-500 dark:text-black-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
+                  </svg>
+                  <p class="mb-2 text-xl text-black-500 dark:text-black-400"><span class="font-bold">Click to upload</span> or drag and drop</p>
+                  <p class="text-lg text-black-500 dark:text-black-400">PNG, JPG or GIF</p>
+              </div>
+              <input id="dropzone-file" type="file" class="hidden" onChange={(e) => setImageFile(e.target.files[0])}/>
+          </label>
+        </div> 
 
-          <div class="flex items-center justify-center">
+        <div class="flex items-center justify-center">
           <button onClick={() => submitImage()} class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-white-400 rounded shadow m-3 px-7 pb-2.5 pt-3 text-lg">
             Submit
           </button>
-          </div>
+        </div>
 
-      
-          {/* </div> */}
       </div>
 
       <div className="container mx-auto">
-
         {errors?.length > 0 && (
           <div className='flex flex-col items-center'>
             <div>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,7 +4,24 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes:{
+        'change-font':{
+          '0%':{ fontFamily: 'Handlee'},
+          '20%':{ fontFamily: 'Finger Paint'},
+          '40%':{ fontFamily: 'Sacramento'},
+          '60%':{ fontFamily: 'Shadows Into Light Two'},
+          '80%':{ fontFamily: 'Reenie Beanie'},
+          // '100%':{ fontFamily: 'sans-serif'}
+        }
+      },
+      animation:{
+        'change-font': 'change-font 3s linear infinite'
+      }, 
+      fontFamily: {
+        fontTitle: ['Handlee', 'Sacramento', 'Finger Paint', 'Shadows Into Light Two', 'Reenie Beanie']
+      }
+    },
   },
   plugins: [],
 }
