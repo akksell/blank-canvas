fd703e216b3bcc0191c4b9129ce7de4163059025
julianaLeano <julexd125@gmail.com>
2023-11-28 18:30:38 -0600

    initial font change and color sizes for title and upload text

--- client/src/App.js
+++ client/src/App.js
@@ -38,7 +38,7 @@ function App() {
 
   return (
     <div className="App">
-      <div class="flex grid grid-rows-4 grid-cols-1 items-center justify-center content-between pt-64 pl-40 pr-40 w-full bg-screen bg-center h-screen bg-[url('/public/header_canvas.jpg')] ">
+      <div class="flex grid grid-rows-4 grid-cols-1 items-center justify-center content-between pt-64 w-full bg-screen bg-center h-screen bg-[url('/public/header_canvas.jpg')] ">
           {/* <div class="flex grid grid-rows-2 grid-cols-1 items-center justify-center content-between pt-72"> */}
           {/* ^^^bg-gray-900 bg-opacity-50 */}
             <div class="text-center">
@@ -51,11 +51,11 @@ function App() {
             <label for="dropzone-file" class="flex flex-col items-center justify-center h-64 w-1/2 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-transparent m-3">
               {/*<label for="dropzone-file" class="flex flex-col items-center justify-center w-1/2 h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600 m-3">*/}
                 <div class="flex flex-col items-center justify-center pt-5 pb-6">
-                    <svg class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                    <svg class="w-8 h-8 mb-4 text-black-500 dark:text-black-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
                     </svg>
-                    <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
-                    <p class="text-xs text-gray-500 dark:text-gray-400">PNG, JPG or GIF</p>
+                    <p class="mb-2 text-md text-black-500 dark:text-black-400"><span class="font-bold">Click to upload</span> or drag and drop</p>
+                    <p class="text-sm text-black-500 dark:text-black-400">PNG, JPG or GIF</p>
                 </div>
                 <input id="dropzone-file" type="file" class="hidden" onChange={(e) => setImageFile(e.target.files[0])}/>
             </label>
